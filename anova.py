46c679b926205e2e614ab480af93eec3dab442af
Barbera <barberacoosje@gmail.com>
2021-12-30 15:51:34 +0100

    update with new ml models and one-hot-encoding

--- anova.py
+++ anova.py
@@ -1,12 +1,13 @@
 ## Import packages
 import pandas as pd
 import numpy as np
-from sklearn import feature_selection, linear_model, metrics, model_selection
+from sklearn import feature_selection, linear_model, metrics, model_selection, tree, ensemble, decomposition, preprocessing
 import seaborn as sns
 import matplotlib.pyplot as plt
 from imblearn import over_sampling
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
+
 import time
 
 
@@ -111,10 +112,12 @@ for column in df:
         print("Removing constant column: ", column)
         df = df.drop(column, axis=1)
 
-
 df["FLAG_OWN_CAR"] = df["FLAG_OWN_CAR"].replace(['Y', 'N'], [1, 0])
 df["FLAG_OWN_REALTY"] = df["FLAG_OWN_REALTY"].replace(['Y', 'N'], [1, 0])
 df["EMERGENCYSTATE_MODE"] = df["EMERGENCYSTATE_MODE"].replace(['Yes', 'No'], [1, 0])
+
+encoder = preprocessing.OneHotEncoder(handle_unknown='ignore')
+
 
 for column in df:
     if df[column].dtype == object:
@@ -122,27 +125,18 @@ for column in df:
         # print(df[column].unique())
         # print(df[column].value_counts(ascending=False))
         # print("Number of missing values for " + str(column) + ": " + str(df.isnull().sum()[column]) + "\n\n")
-        df[column] = df[column].replace(df[column].unique().tolist(), [*range(1, len(df[column].unique()) + 1)])
+        
+        # one-hot encoding
+        df = pd.concat([df,pd.get_dummies(df[column], prefix=column)],axis=1)
+        df.drop([column],axis=1, inplace=True)
 
+        # label encoding
+        # df[column] = df[column].replace(df[column].unique().tolist(), [*range(1, len(df[column].unique()) + 1)])
 
 print("\t---MICE...---")
 # df = replace_nans_with_mode(df)
 df = apply_MICE(df)
 
-
-
-# code for one-hot encoding
-# # creating initial dataframe
-# bridge_types = ('Arch','Beam','Truss','Cantilever','Tied Arch','Suspension','Cable')
-# bridge_df = pd.DataFrame(bridge_types, columns=['Bridge_Types'])
-# # generate binary values using get_dummies
-# dum_df = pd.get_dummies(bridge_df, columns=["Bridge_Types"], prefix=["Type_is"] )
-# # merge with main df bridge_df on key values
-# bridge_df = bridge_df.join(dum_df)
-# bridge_df
-
-
-# print(df['CODE_GENDER'].value_counts(ascending=False))
 
 # setting predictors and targets
 target = df["TARGET"]
@@ -173,12 +167,44 @@ X = df.iloc[:, 2:]
 #             annot=False, cbar_kws={"shrink": .7})
 # plt.show()
 
-
-print("\t---Perform cross-validation...---")
 # transform data to numpy arrays
 y = np.array(target)
 x = np.array(X)
 
+# print("\t---perform PCA...---")
+# covariance_matrix = np.cov(x)
+# eigen_values, eigen_vectors = np.linalg.eig(covariance_matrix)
+
+# variance_explained = []
+# for eigen_value in eigen_values:
+#     variance_explained.append((eigen_value/sum(eigen_values)*100))
+
+# total_variance_explained = []
+# for index, value in enumerate(variance_explained):
+#     total_variance_explained.append(sum(variance_explained[:index+1]))
+
+# fig, ax = plt.subplots()
+# variance_table = pd.DataFrame(columns=['PCs','totalVariance'])
+
+# for index in range(0,60):
+#     variance_table = variance_table.append({'PCs': int(index), 'totalVariance': np.round(total_variance_explained[index-1], decimals=2)}, ignore_index=True)
+
+# plt.style.use("ggplot")
+# plt.plot(variance_explained)
+# plt.plot(total_variance_explained)
+# plt.xlim(0,250)
+# plt.title("Variance explained with increasing number of principal components")
+# plt.xlabel("Number of PCs")
+# plt.ylabel("Variance explained (%)")
+# plt.legend(["Variance explained per PC", "Total variance explained"])
+# plt.show()
+
+# k = 200
+# pca_func = decomposition.PCA(n_components=k)
+# x_pca = pca_func.fit_transform(x)
+# print(np.array([x_pca.explained_variance_ratio_[:i].sum() for i in range(1, k+1)]).round(2))
+
+print("\t---Perform cross-validation...---")
 # implement k-fold cross-validation
 k = 2
 kf = model_selection.KFold(n_splits=k, shuffle=True, random_state=0)
@@ -194,12 +220,22 @@ for train_i, test_i in kf.split(x):
 
     # make a simple logistic regression model
     print("\t---perform simple logistic regression...---")
-    model = linear_model.LogisticRegression(solver="liblinear", random_state=0).fit(x_smote, y_smote)
+    modelLogReg = linear_model.LogisticRegression(solver="liblinear", random_state=0).fit(x_smote, y_smote)
+    print(metrics.classification_report(y_test, modelLogReg.predict(x_test)))
+
+    print("\t---making decision tree...---")
+    modelDecTree = tree.DecisionTreeClassifier(criterion="gini", splitter="best").fit(x_smote, y_smote)
+    print(metrics.classification_report(y_test, modelDecTree.predict(x_test)))
+
+    print("\t---random forests...---")
+    modelRanFor = ensemble.RandomForestClassifier().fit(x_smote, y_smote)
+    print(metrics.classification_report(y_test, modelRanFor.predict(x_test)))
 
     # calculate the confusion matrix
-    print(metrics.confusion_matrix(y_test, model.predict(x_test)))
+    # print(metrics.confusion_matrix(y_test, modelLogReg.predict(x_test)))
+    # # calculate the confusion matrix
+    # print(metrics.confusion_matrix(y_test, modelDecTree.predict(x_test)))
 
-    print(metrics.classification_report(y_test, model.predict(x_test)))
 
 # Precision: How many retrieved items are relevant?
 # Recall: How many relevant items are retrieved?
