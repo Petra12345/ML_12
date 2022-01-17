24d9a10f16da30a0fb6f7b0399a7cd6831c6d1cb
Sverre Brok <sverrebrok@gmail.com>
2022-01-17 20:48:43 +0100

    Implemented cross-validation in a correct way

--- Functions/model_funcs.py
+++ Functions/model_funcs.py
@@ -1,9 +1,14 @@
+import numpy as np
+import pandas as pd
 from imblearn import over_sampling
 from matplotlib import pyplot as plt
 import seaborn as sns
 
 from sklearn import feature_selection, model_selection, linear_model, tree, metrics, ensemble
 from sklearn.pipeline import make_pipeline
+
+from Functions.preprocessing_funcs import remove_constant_columns, apply_MICE, \
+    normalize_data, perform_pca
 
 
 def anova_feature_selection(X, target, num_features=20):
@@ -42,47 +47,76 @@ def print_baseline(y_test):
     num_0 = len(y_test) - num_1
 
 
+def make_lin_model(solver="liblinear"):
+    return linear_model.LogisticRegression(solver=solver, random_state=0)
 
 
-def cross_validation(x, y, k=2):
+def make_decision_tree_model(criterion="gini", splitter="best"):
+    return tree.DecisionTreeClassifier(criterion=criterion, splitter=splitter)
+
+
+def plot_decision_tree(model):
+    tree.plot_tree(model)
+    plt.show()
+
+
+def make_random_forest_model():
+    return ensemble.RandomForestClassifier()
+
+
+def cross_validation(data_raw, models_dict, k=2):
     """
     Perform cross_validation given features x and target y.
-    :param x:   The features
-    :param y:   The target
-    :param k:   Variable to determine k-fold cross validation
+    :param training_data_raw:
+    :param models_dict:
+    :param k:                   Variable to determine k-fold cross validation
     """
     kf = model_selection.KFold(n_splits=k, shuffle=True, random_state=0)
+    iter = 0
 
-    for train_i, test_i in kf.split(x):
-        x_train, x_test = x[train_i, :], x[test_i, :]
-        y_train, y_test = y[train_i], y[test_i]
+    for train_i, val_i in kf.split(data_raw):
+        iter += 1
+        print(f"##### Iteration {iter} #####")
+        training_data, validation_data = data_raw.iloc[train_i,], data_raw.iloc[val_i,]
 
-        # predict all zero:
-        print_baseline(y_test)
+        print("\t---Data preprocessing---")
+        training_data, rm_columns = remove_constant_columns(training_data)
+        validation_data.drop(rm_columns, axis=1)
 
-        # balance out data with SMOTE
-        print("\t---perform SMOTE...---")
+        one_hot_encoded_training_data = pd.get_dummies(training_data)
+        one_hot_encoded_validation_data = pd.get_dummies(validation_data)
+        training_data, validation_data = one_hot_encoded_training_data.align(one_hot_encoded_validation_data,
+                                                                             join='right', axis=1)
+
+        # Apply MICE and standardize cols
+        print("\t---Apply MICE---")
+        training_data, imp_median, imp_mode = apply_MICE(training_data, fit=True)
+        validation_data, _, _ = apply_MICE(validation_data, fit=False, imp_median=imp_median, imp_mode=imp_mode)
+        training_data, norm_columns = normalize_data(training_data)
+        validation_data[norm_columns] = validation_data[norm_columns].apply(
+            lambda x: (x - x.min()) / (x.max() - x.min()))
+
+        # setting predictors and targets
+        y_train = np.array(training_data["TARGET"])
+        y_validation = np.array(validation_data["TARGET"])
+        x_train = np.array(training_data.iloc[:, 2:])
+        x_validation = np.array(validation_data.iloc[:, 2:])
+
+        # PCA
+        print("\t---PCA---")
+        x_train, pca_func = perform_pca(x_train)
+        x_validation = pca_func.transform(x_validation)
+
         smote = over_sampling.SMOTE(random_state=0)
         x_smote, y_smote = smote.fit_resample(x_train, y_train)
+        # We don't have to apply smote on the validation data
 
-        # make a simple logistic regression model
-        print("\t---perform simple logistic regression...---")
-        modelLogReg = linear_model.LogisticRegression(solver="liblinear", random_state=0).fit(x_smote, y_smote)
-        print(metrics.classification_report(y_test, modelLogReg.predict(x_test)))
-        print(metrics.confusion_matrix(y_test, modelLogReg.predict(x_test)))
-        print(metrics.f1_score(y_test, modelLogReg.predict(x_test)))
+        for key, model in models_dict.items():
+            print(f"\t\t---perform {key}...---")
+            model.fit(x_smote, y_smote)
+            print(metrics.confusion_matrix(y_validation, model.predict(x_validation)))
+            print(metrics.f1_score(y_validation, model.predict(x_validation)))
+            print(metrics.classification_report(y_validation, model.predict(x_validation)))
 
-        print("\t---making decision tree...---")
-        modelDecTree = tree.DecisionTreeClassifier(criterion="gini", splitter="best").fit(x_smote, y_smote)
-        print(metrics.classification_report(y_test, modelDecTree.predict(x_test)))
-        # tree.plot_tree(modelDecTree)
-        # plt.show()
-
-        print("\t---random forests...---")
-        modelRanFor = ensemble.RandomForestClassifier().fit(x_smote, y_smote)
-        print(metrics.classification_report(y_test, modelRanFor.predict(x_test)))
-
-        # calculate the confusion matrix
-        # print(metrics.confusion_matrix(y_test, modelLogReg.predict(x_test)))
-        # # calculate the confusion matrix
-        # print(metrics.confusion_matrix(y_test, modelDecTree.predict(x_test)))
+    # Precision: How many retrieved items are relevant?
+    # Recall: How many relevant items are retrieved?
--- Functions/preprocessing_funcs.py
+++ Functions/preprocessing_funcs.py
@@ -17,87 +17,19 @@ def load_data(file_name='Data/application_data/application_data.csv'):
     return pd.read_csv(file_name)
 
 
-def standardize_continuous_numeric_cols(df):
-    """
-    Function for standardizing all continuous variables in the dataframe.
-    :param df:  The dataframe to be standardized
-    :return:    The dataframe that has its continuous columns standardized
-    """
-    for column in df:
-        if df[column].dtype == float:
-            # TODO: WAT GEBEURT HIER MET MISSING DATA?
-            mean_col = np.mean(df[column])
-            stddev_col = np.std(df[column])
-            # print("column " + column + " has mean: " + str(mean_col) + " and stdev " + str(stddev_col))
-
-            df[column] = (df[column] - mean_col) / stddev_col
-    return df
-
-
-def remove_columns_missing_values(df, missing_cut_off=0.6):
-    """
-    Removes all columns (variables) that have at least a percentage of missing values
-    according to a specified cut-off.
-    :param df:                  The dataframe to remove columns from
-    :param missing_cut_off:     The cut-off percentage of missing values
-    :return:                    A dataframe with columns removed.
-    """
-    count_missing_per = df.isnull().sum()
-    count_missing_per.describe()
-    count_missing_per = count_missing_per.div(len(df))
-
-    missing_data_cut_off = count_missing_per[count_missing_per < missing_cut_off]
-    labels_included_variables = missing_data_cut_off.keys()
-    data_without_columns_missing = df[labels_included_variables]
-    return data_without_columns_missing
-
-
 def remove_constant_columns(df):
     """
     Removes all constant columns, that is, columns that contain only one value.
     :param df:  The dataframe to be transformed
     :return:    The transformed dataframe with constant columns removed
     """
+    rm_columns = []
     for column in df:
         if len(df[column].unique()) == 1:
             print("Removing constant column: ", column)
             df = df.drop(column, axis=1)
-    return df
-
-
-def one_hot_encode_categorical_cols(df):
-    """
-    One-hot encode all categorical columns of the given dataframe.
-    :param df:  The dataframe
-    :return:    The transformed dataframe with all categorical columns one-hot encoded
-    """
-    for col in ["FLAG_OWN_CAR", "FLAG_OWN_REALTY"]:
-        df[col] = df[col].replace(['Y', 'N'], [1, 0])
-
-    for col in ["EMERGENCYSTATE_MODE"]:
-        df[col] = df[col].replace(['Yes', 'No'], [1, 0])
-
-    for column in df:
-        if df[column].dtype == object:
-            # print(column)
-            # print(df[column].unique())
-            # print(df[column].value_counts(ascending=False))
-            # print("Number of missing values for " + str(column) + ": " + str(df.isnull().sum()[column]) + "\n\n")
-
-            df = pd.concat([df, pd.get_dummies(df[column], prefix=column)], axis=1)
-            df.drop([column], axis=1, inplace=True)
-
-    return df
-
-
-def normalize(arr, t_min, t_max):
-    norm_arr = []
-    diff = t_max - t_min
-    diff_arr = max(arr) - min(arr)
-    for i in arr:
-        temp = (((i - min(arr)) * diff) / diff_arr) + t_min
-        norm_arr.append(temp)
-    return norm_arr
+            rm_columns.append(column)
+    return df, rm_columns
 
 
 def normalize_data(df):
@@ -106,21 +38,14 @@ def normalize_data(df):
     :param df:  The dataframe to be normalized
     :return:    The dataframe that has its columns normalized
     """
-    print("---Normalize---")
-    # df_num = df.select_dtypes(include=[np.float])
-    # range_to_normalize = (0, 1)
-    # for column in df:
+    # cols_to_norm = []
+    # for column in df.columns:
     #     if df[column].dtype == float:
-    #         df[column] = normalize(df[column], range_to_normalize[0], range_to_normalize[1])
-    #         print(df[column])
-    #
-    cols_to_norm = []
-    for column in df.columns:
-        if df[column].dtype == float:
-            cols_to_norm.append(column)
+    #         cols_to_norm.append(column)
+    cols_to_norm = df.select_dtypes(include=[np.float])
     df[cols_to_norm] = df[cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
 
-    return df
+    return df, cols_to_norm
 
 
 def make_dataframe_MICE(df, fill_in):
@@ -131,33 +56,53 @@ def make_dataframe_MICE(df, fill_in):
     :return:            The dataframe with the missing values filled in according to the fill_in method
     """
     float_labels = df.columns.tolist()
-    data_mice = fill_in.fit_transform(df)
+    print(len(float_labels))
+    print(float_labels)
+    data_mice = fill_in.transform(df)
+
     data_mice = pd.DataFrame(data=data_mice)
+    print(data_mice)
+    print(len(data_mice.columns.tolist()))
+    print(data_mice.columns.tolist())
+    # print(f"Difference: {set(float_labels).symmetric_difference(set(data_mice.columns.tolist()))}")
     data_mice = data_mice.set_axis(float_labels, axis=1, inplace=False)
     return data_mice
 
 
-def apply_MICE(df):
+def apply_MICE(df, fit=False, imp_median=None, imp_mode=None):
     """
     Apply MICE to fill in the missing values in the data frame
-    :param df:  The dataframe
-    :return:    The dataframe with missing values filled in according to MICE.
+    :param fit:
+    :param imp_mode:
+    :param imp_median:
+    :param df:          The dataframe
+    :return: The dataframe with missing values filled in according to MICE.
     """
     start = time.time()
     labels = df.columns.tolist()
-    imp_median = IterativeImputer(max_iter=10, tol=0.001, n_nearest_features=10, initial_strategy='median',
-                                  skip_complete=False, verbose=2, add_indicator=False)
-    imp_mode = IterativeImputer(max_iter=10, tol=0.001, n_nearest_features=10, initial_strategy='most_frequent',
-                                skip_complete=False, verbose=2, add_indicator=False)
 
+    if not fit and ((imp_median is None) or (imp_mode is None)):
+        print("ERROR: Cannot only transform if fitting functions are not given.")
+        imp_median = None
+        imp_mode = None
+
+    if fit:
+        imp_median = IterativeImputer(max_iter=10, tol=0.001, n_nearest_features=10, initial_strategy='median',
+                                      skip_complete=False, verbose=2, add_indicator=False, random_state=0)
+        imp_median = imp_median.fit(df.select_dtypes("float"))
     data_mice_float = make_dataframe_MICE(df.select_dtypes("float"), imp_median)
-    data_mice_cat = make_dataframe_MICE(df.select_dtypes(exclude="float"), imp_mode)
-    data_mice = pd.concat([data_mice_cat, data_mice_float], axis=1)
 
+    if fit:
+        imp_mode = IterativeImputer(max_iter=10, tol=0.001, n_nearest_features=10, initial_strategy='most_frequent',
+                                    skip_complete=False, verbose=2, add_indicator=False, random_state=0)
+        imp_mode.fit(df.select_dtypes(exclude="float"))
+    data_mice_cat = make_dataframe_MICE(df.select_dtypes(exclude="float"), imp_mode)
+
+    data_mice = pd.concat([data_mice_cat, data_mice_float], axis=1)
     data_mice = data_mice.reindex(columns=labels)
     print("Time taken: " + str(time.time() - start) + " sec.\n\n")
 
-    return data_mice
+    return data_mice, imp_median, imp_mode
 
 
 def perform_pca(x, k=0.9):
@@ -196,8 +141,8 @@ def perform_pca(x, k=0.9):
 
     # TODO: miss number of components bepalen aan de hand van expl variance
     pca_func = PCA(n_components=k, svd_solver='full')
-    x_pca = pca_func.fit(x)
-    x_pca = pca_func.fit_transform(x, k)
+    pca_func.fit(x)
+    x_pca = pca_func.transform(x)
     # print(np.array([x_pca.explained_variance_ratio_[:i].sum() for i in range(1, k+1)]).round(2))
     # print(x_pca.explained_variance_ratio_)
     return x_pca, pca_func
