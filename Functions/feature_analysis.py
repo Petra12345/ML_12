f6820bbe9ff37fc1304672b7c41368c5540fb5f0
sopje <Nwsgh1997@>
2022-02-02 11:19:23 +0100

    ready to grid search with correct loss

--- Functions/feature_analysis.py
+++ Functions/feature_analysis.py
@@ -1,5 +1,6 @@
 from treeinterpreter import treeinterpreter as ti
 
 def interpret_RF(RF_model, test_data):
-    prediction, bias, contributions = ti.predict(RF_model, testX)
+    prediction, bias, contributions = ti.predict(RF_model, test_data)
+    return prediction, bias, contributions
 
