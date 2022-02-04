06a24be9f1db240b56d0594d05e4194fc490c7aa
Sverre Brok <sverrebrok@gmail.com>
2022-02-04 14:58:10 +0100

    plots categorical variables

--- Somethings/categories_variables.py
+++ Somethings/categories_variables.py
@@ -1,8 +1,5 @@
 # Import packages
-import itertools
-
-import numpy as np
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 from Functions.model_funcs import *
 from Functions.preprocessing_funcs import *
@@ -10,6 +7,8 @@ from Functions.preprocessing_funcs import *
 # Load data and some initial data processing
 print("---Load data---")
 df = load_data()
+
+#%%
 column_names = list(df.columns)
 
 # make categories
@@ -37,10 +36,39 @@ categories = ['Personal', 'Contract', 'Assets', 'Work', 'Housing', 'Miscellaneou
 
 num_variables, categories = (list(t) for t in zip(*sorted(zip(num_variables, categories))))
 
-fig = plt.figure(figsize=(10, 4))
-plt.axes([0.15, 0.12, 0.8, 0.8])
+fig = plt.figure(figsize=(8, 4))
+plt.axes([0.3, 0.18, 0.67, 0.77])
 plt.barh(categories, num_variables)
-plt.ylabel("Parameter categories")
-plt.xlabel("No. of parameters")
+plt.ylabel("Feature categories")
+plt.xlabel("No. of features")
+plt.xlim(0, 62)
+axes = plt.gca()
+axes.yaxis.label.set_size(24)
+axes.xaxis.label.set_size(24)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+for i, v in enumerate(num_variables):
+    axes.text(v + 1, i - 0.2, str(v), fontweight='bold', fontsize=16)
 
+plt.savefig("catgor_var.pdf")
+plt.show()
+
+#%%
+
+percent_missing = df.isnull().sum() * 100 / len(df)
+
+#%%
+plt.axes([0.15, 0.15, 0.8, 0.8])
+bins = (np.arange(11) - 0.5) * 10
+plt.hist(percent_missing, bins)
+plt.xticks((np.arange(11) * 10), fontsize=14)
+plt.yticks(fontsize=14)
+plt.xlabel("% of missing values")
+plt.ylabel("Feature count")
+axes = plt.gca()
+axes.xaxis.label.set_size(20)
+axes.yaxis.label.set_size(20)
+
+
+plt.savefig("feature_analysis_hist.pdf")
 plt.show()
